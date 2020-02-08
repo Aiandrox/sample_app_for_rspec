797db5444e8d186d24dbaef02595ec5cc5422475
Aiandrox <aiandrox7@gmail.com>
2020-02-08 12:33:15 +0900

    [Fix] 空行削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,7 +9,7 @@ FactoryBot.define do
     factory :user_with_tasks do
       transient { tasks_count { 5 } }
       after(:create) do |user, evaluator|
-        create_list(:task, evaluator.tasks_count, user: user )
+        create_list(:task, evaluator.tasks_count, user: user)
       end
     end
   end
