5da0d933d1c7cb2519f967ab231282e2755964d2
Aiandrox <aiandrox7@gmail.com>
2020-02-08 13:51:24 +0900

    [Add] loginヘルパーにログイン成功の確認を追加

--- spec/support/login_helpers.rb
+++ spec/support/login_helpers.rb
@@ -5,5 +5,6 @@ module LoginHelpers
     fill_in 'Email', with: user.email
     fill_in 'Password', with: 'password'
     click_button 'Login'
+    expect(page).to have_content 'Login successful'
   end
 end
